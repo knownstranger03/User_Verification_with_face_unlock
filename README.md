efd174a3b5fa8482e3c6c14cc3f237930140fc63
Krishna Prasad <Sidhuprasad0304@gmail.com>
2019-11-17 18:50:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# User_Verification_with_face_unlock
+ This is a beta version of an user verification application for windows.  Ver-1.0
---
+++ execute.bat
@@ -0,0 +1,1 @@
+"C:\Users\skris\AppData\Local\Programs\Python\Python36\python.exe" "D:\Projects\User Verification\lock_unlock_face_recognition.py"
